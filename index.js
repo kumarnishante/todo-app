0c814fa50a71053c8f764249c33f6367c4c0e814
kumarnishante <krnishant805110@gmail.com>
2025-03-31 14:00:58 +0530

    Added BE

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 
 const express = require('express');
+const { createTodo, updateTodo } = require('./types');
 const app = express();
 
 app.use(express.json());
@@ -14,7 +15,14 @@ app.use(express.json());
 //}
 
 app.post("/todo", function(req, res) {
-
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+    }
+    // put it in mongodb
 })
 
 app.get("todos", function(req, res) {
@@ -22,5 +30,13 @@ app.get("todos", function(req, res) {
 })
 
 app.put("/completed", function(req, res) {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
 
+    if(!parsedPayload.success){
+        req.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
 })
