f05db7d8ef990f1a8f33d36cc821a7012e8b26f5
kumarnishante <krnishant805110@gmail.com>
2025-03-31 12:52:04 +0530

    New year

---
+++ index.js
@@ -0,0 +1,26 @@
+
+// write basic express boilerplate code,
+// with express.json() middleware
+
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+//{
+    // title: string;
+    // description: string;
+//}
+
+app.post("/todo", function(req, res) {
+
+})
+
+app.get("todos", function(req, res) {
+
+})
+
+app.put("/completed", function(req, res) {
+
+})
