f334de0ff10c61141a2edd2925180008ab85c4f7
kumarnishante <krnishant805110@gmail.com>
2025-04-01 22:43:01 +0530

    added backend and frontend

---
+++ backend/db.js
@@ -0,0 +1,31 @@
+/*
+    Todo{
+        title: string; 
+        description: string;
+        completed: boolean
+    }
+*/
+
+
+
+const mongoose = require("mongoose");
+// mongodb url handy
+// mongodb+srv://admin:vLyJcyrjxCHtTpv6@cluster0.5uv3y.mongodb.net/
+// .env
+
+mongoose.connect("mongodb+srv://admin:vLyJcyrjxCHtTpv6@cluster0.5uv3y.mongodb.net/")
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+
+const todo = mongoose.model('todos', todoSchema);
+
+module.exports = {
+    todo
+}
